2bff276c0b232b630c210d680bd9005c02c89613
claudioolguin <cl.olguin@duocuc.cl>
2025-10-28 01:39:48 -0300

    Delete app directory

--- app/src/main/java/com/example/proyectorulaygame/ChallengeModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.proyectorulaygame
-// Clase de datos simple para representar un reto del juego.
-// Usamos data class para obtener automáticamente métodos útiles como toString().
-data class Challenge(
-    val id: Int = 0,
-    val type: String,  // Ejemplo: "Verdad", "Reto", "Beber"
-    val mode: String, // Modo de juego (Ej: "Normal", "Hot", "Cultura Chupística")
-    val text: String
-)
--- app/src/main/java/com/example/proyectorulaygame/activity_reto_o_bebe_layout.java
+++
@@ -1,6 +0,0 @@
-package com.example.proyectorulaygame;
-
-import android.app.Activity;
-
-public class activity_reto_o_bebe_layout extends Activity {
-}
